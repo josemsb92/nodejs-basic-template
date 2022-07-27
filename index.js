1df1d9b2e952cbd3236f4056dcdd9a57f65006c3
josemsb92 <josemsb92@gmail.com>
2022-07-27 23:11:17 +0200

    bodyParser added on middlewares

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
-
+app.use(bodyParser.json());
+require("./middlewares/conection");
 //All routes starting with /api verify that they receive a valid token in the header.
 //app.use("/api", authentication, require("./routes"));
 
