d439129d7819c85e55a885f77498d660ca7a687e
josemsb92 <josemsb92@gmail.com>
2022-07-28 22:47:47 +0200

    login route

--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,5 @@ router.get("/all", require("../controllers/getUsers"));
 router.get("/:id", require("../controllers/getById"));
 router.post("/post", require("../controllers/postUser"));
 router.delete("/delete", require("../controllers/deleteUser"));
+router.post("/login", require("../controllers/login"));
 module.exports = router;
