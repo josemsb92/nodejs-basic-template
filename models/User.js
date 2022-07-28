cdea58ff563705e8ef37818f55ac9b6b5a571c85
josemsb92 <josemsb92@gmail.com>
2022-07-28 22:47:36 +0200

    schema transform now delete _id

--- models/User.js
+++ models/User.js
@@ -8,6 +8,15 @@ const userSchema = new Schema({
   comments: [{ body: String, date: Date }],
   date: { type: Date, default: Date.now },
 });
+//El metodo transform nos permite transformar los datos que nos devuelve la petición, de esta manera podemos eliminar
+//el _id que devuelve mongoose y guardamos el valor de _id en id, para que sea más fácil de leer
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.password;
+    delete returnedObject._id;
+  },
+});
 
 const User = model("User", userSchema);
 module.exports = User;
