23181f443ef181881e34ce2896b943b7f3c94b34
josemsb92 <josemsb92@gmail.com>
2022-07-28 22:46:55 +0200

    hash password with bcrypt

--- controllers/postUser.js
+++ controllers/postUser.js
@@ -1,9 +1,15 @@
 const userManager = require("../managers/userManager");
-
+const bcrypt = require("bcrypt");
 async function postUser(req, res) {
   const { body } = req;
-  const user = await userManager.createUser(body.username, body.password);
-  user.save();
+  const saltRounds = 10;
+  const password = await bcrypt.hash(body.password, saltRounds);
+  const user = await userManager.createUser(body.username, password);
+  try {
+    user.save();
+  } catch (error) {
+    console.log(error);
+  }
   res.status(201).json({ user: user }).end();
 }
 
